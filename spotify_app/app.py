2f092d44e5555ade4e7b02fb48f8fdfc925d1706
Johne-DuChene <85814636+Johne-DuChene@users.noreply.github.com>
2022-02-04 04:44:48 -0800

    Pretty much everything is done, it takes a song ID in the url and returns a list of IDs of songs that are similar.

--- spotify_app/app.py
+++ spotify_app/app.py
@@ -7,11 +7,19 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+
 # useful datatype- defaultdicts don't throw key errors.
 from collections import defaultdict
 import pandas as pd
 
 load_dotenv()
+APP = Flask(__name__)
+db = SQLAlchemy(APP)
+APP.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASEURI")
 
 # load the .env file and return credentials
 def return_credentials():
@@ -32,6 +40,40 @@ def create_cursor(client_id, client_secret, redirect_uri):
         client_secret = client_secret, redirect_uri = redirect_uri,
         scope = "user-read-recently-played"))
     return sp
+
+def epic_predictor(input_track_key):
+
+  ## Convert "input_track_key" to the index of the song (the song's position
+  ## in the dataframe).
+  # imports data to a dataframe from the csv
+  data = pd.read_csv("https://raw.githubusercontent.com/Lambda-Spotify-Song-Suggester-3/datascience/master/kaggle_data/encoded.csv")
+  df = data.copy()
+  dictionary = df[["artist_name", "track_name", "track_key", "track_id"]]
+  # Drop
+  df = df.drop(columns=['artist_name','track_id', 'track_name','track_key', 'duration_ms', 'mode', 'loudness', 'time_signature'])
+  # Scale the data
+  scaler = StandardScaler()
+  # the scaled dataframe
+  df_s = scaler.fit_transform(df)
+
+  input_dictionary_entry = dictionary[dictionary['track_key']==input_track_key]
+  input_index = input_dictionary_entry.index[0]
+
+  ## Nearest Neighbors model
+  nn = NearestNeighbors(n_neighbors=10, algorithm='kd_tree')
+  nn.fit(df_s)
+
+  neighbor_predictions = nn.kneighbors([df_s[input_index]])
+
+  ## This is a list of the INDEXES of the songs
+  list_of_predictions = neighbor_predictions[1][0].tolist()
+
+  ten_similar_tracks = []
+  for item in list_of_predictions:
+    track_hash = dictionary['track_id'].iloc[item]
+    ten_similar_tracks.append(track_hash)
+
+  return ten_similar_tracks
 
 # query function for missing songs
 def find_song(name, year):
@@ -65,3 +107,9 @@ def find_song(name, year):
         song_data[key] = value
 
     return pd.DataFrame(song_data).to_csv("song_data.csv")
+
+# main route
+@APP.route("/")
+def main():
+    track_key = int(request.args["track_key"])
+    return str(epic_predictor(track_key))
